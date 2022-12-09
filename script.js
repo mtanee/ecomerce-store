f953f8308c8f0836eb5fac1dd17a47577d344bab
mtanee <114029687+mtanee@users.noreply.github.com>
2022-12-10 03:14:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click',() =>{
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
+}
+
+/* Bottom to Top button */
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    }else{
+        toTop.classList.remove("active");
+    }
+})
